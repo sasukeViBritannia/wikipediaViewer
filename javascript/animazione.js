24967494ca2910d5e27813d36192323cb305af05
Ale <ale@aleLinux>
2017-05-09 23:08:13 +0200

    Aggiunta richiesta ajax e testata

--- javascript/animazione.js
+++ javascript/animazione.js
@@ -6,6 +6,24 @@ $(document).ready(function() {
         window.open('https://en.wikipedia.org/wiki/Special:Random');
     });
 
+    $('#invia').on('click', function(){
+
+    	$.ajax({
+    		url: 'https://en.wikipedia.org///w/api.php?action=query&format=json&list=search&srsearch=Juventus',
+    		type: 'GET',
+    		dataType: 'jsonp',
+    	})
+    	.done(function() {
+    		console.log("success");
+    	})
+    	.fail(function() {
+    		console.log("error");
+    	})
+    	.always(function() {
+    		console.log("complete");
+    	});
+    	
+    });
 });
 
 /*Chiamate ajax
@@ -14,4 +32,5 @@ list di tipo search
 rssearch la stringa da ricercare
 formato tipo json
 controllare le pagine dal titolo e fare encoding uri
---da verificare se possibile aggiungere piccola didascalia*/
+--da verificare se possibile aggiungere piccola didascalia--
+ATTENZIONE all'evento sul pulsante all'internodella form*/
