71ffd13c88b941be2e57f0e278528cf21b175ebb
ale <ale@ubuntu>
2017-05-09 18:08:05 +0200

    Aggiunta nota

--- javascript/animazione.js
+++ javascript/animazione.js
@@ -2,9 +2,16 @@
 $(document).ready(function() {
 
 
-	$('#casuale').on('click', function(){
-		window.open('https://en.wikipedia.org/wiki/Special:Random');
-	});
+    $('#casuale').on('click', function() {
+        window.open('https://en.wikipedia.org/wiki/Special:Random');
+    });
 
 });
 
+/*Chiamate ajax
+con parametri action di tipo query
+list di tipo search
+rssearch la stringa da ricercare
+formato tipo json
+controllare le pagine dal titolo e fare encoding uri
+--da verificare se possibile aggiungere piccola didascalia*/
