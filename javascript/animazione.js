6582a61f5289e46eb5eb003480f5e4806e9c6d4c
ale <ale@ubuntu>
2017-05-11 12:12:40 +0200

    Aggiunto creazione link per riferimento pagina wikipedia

--- javascript/animazione.js
+++ javascript/animazione.js
@@ -2,27 +2,34 @@
 $(document).ready(function() {
 
 
+    var link = '';
+
     $('#casuale').on('click', function() {
-        window.open('https://en.wikipedia.org/wiki/Special:Random');
+        window.open('https://it.wikipedia.org/wiki/Special:Random');
     });
 
-    $('#invia').on('click', function(){
+    $('#invia').on('click', function() {
 
-    	$.ajax({
-    		url: 'https://en.wikipedia.org///w/api.php?action=query&format=json&list=search&srsearch=Juventus',
-    		type: 'GET',
-    		dataType: 'jsonp',
-    	})
-    	.done(function() {
-    		console.log("success");
-    	})
-    	.fail(function() {
-    		console.log("error");
-    	})
-    	.always(function() {
-    		console.log("complete");
-    	});
-    	
+        $.ajax({
+                url: 'https://it.wikipedia.org///w/api.php?action=query&format=json&list=search&srsearch=Juventus',
+                type: 'GET',
+                dataType: 'jsonp',
+            })
+            .done(function(data) {
+                console.log("success");
+                link = data.query.search[0].title;
+                console.log(link);
+                var codificato = encodeURI(link);
+                var nuovoLink = $('<a href="https://it.wikipedia.org/wiki/'+codificato+'" target="_blank">Indirizzo trovato</a>');
+                $('.contenitorePrincipale').append(nuovoLink);
+            })
+            .fail(function() {
+                console.log("error");
+            })
+            .always(function() {
+                console.log("complete");
+            });
+
     });
 });
 
