71dcfd3e857d0e4a7bfdbd3044aa66b62a2edad7
ale <ale@ubuntu>
2017-05-14 18:15:50 +0200

    Terminata parte js da sistemare espandi una volta per riga

--- javascript/animazione.js
+++ javascript/animazione.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 
 
-    /*var link = '';*/
+    var link = '';
 
     /*Apertura pagina casuale in wikipedia*/
     $('#casuale').on('click', function() {
@@ -14,7 +14,7 @@ $(document).ready(function() {
         var testo = $('input[name="ricerca"]').val();
         window.alert('Form attivata con testo: ' + testo);
         $.ajax({
-                url: 'https://it.wikipedia.org///w/api.php?action=query&format=json&list=search&utf8=1&srsearch=' + testo,
+                url: 'https://en.wikipedia.org///w/api.php?action=query&format=json&list=search&utf8=1&srsearch=' + testo,
                 type: 'GET',
                 dataType: 'jsonp',
             })
@@ -22,14 +22,14 @@ $(document).ready(function() {
                 console.log("success");
                 var trovati = data.query.searchinfo.totalhits;
                 if (trovati > 0) {
-                    for (var i = 0; i < 9; i++) {
-                        var link = data.query.search[i].title;
+                    for (var i = 0; i < 6; i++) {
+                        link = data.query.search[i].title;
                         var snippet = data.query.search[i].snippet;
                         var codificato = encodeURI(link);
                         console.log(link);
                         $('.risultati').find('div').eq(i).slideDown('slow').find('p').eq(0).text(link);
                         $('.risultati').find('div').eq(i).slideDown('slow').find('p').eq(1).html(snippet);
-                        $('.risultati').find('div').eq(i).slideDown('slow').find('p').eq(2).find('a').attr({'href': 'https://it.wikipedia.org/wiki/'+codificato, 'target': '_blank'});
+                        $('.risultati').find('div').eq(i).slideDown('slow').find('p').eq(2).find('a').attr({ 'href': 'https://en.wikipedia.org/wiki/' + codificato, 'target': '_blank' });
                     }
 
                 } else {
@@ -45,10 +45,9 @@ $(document).ready(function() {
             });
     });
 
-    $('.card').find('p').last().children().on('click', function() {
+    $('.espandi').on('click', function() {
         $(this).toggleClass('testoNonEspanso testoEspanso');
-   
+        $(this).closest('div').children('p').eq(1).toggle('300ms');
     });
 
 });
-
