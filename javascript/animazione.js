ef09e1e02d68c0135eb1adcdc8f333795514040e
ale <ale@ubuntu>
2017-05-14 13:33:28 +0200

    Aggiunti tutti i contenitori delle risposte, da fare animazione slide

--- javascript/animazione.js
+++ javascript/animazione.js
@@ -2,7 +2,8 @@
 $(document).ready(function() {
 
 
-    var link = '';
+    /*var link = '';*/
+
     /*Apertura pagina casuale in wikipedia*/
     $('#casuale').on('click', function() {
         window.open('https://it.wikipedia.org/wiki/Special:Random');
@@ -21,11 +22,14 @@ $(document).ready(function() {
                 console.log("success");
                 var trovati = data.query.searchinfo.totalhits;
                 if (trovati > 0) {
-                    for (var i = 0; i < 1; i++) {
-                        link = data.query.search[i].title;
+                    for (var i = 0; i < 9; i++) {
+                        var link = data.query.search[i].title;
+                        var snippet = data.query.search[i].snippet;
+                        var codificato = encodeURI(link);
                         console.log(link);
-                        /*var codificato = encodeURI(link);*/
-                        $('.risultati').find('div').eq(i).slideDown('slow').find('p').first().text(link);
+                        $('.risultati').find('div').eq(i).slideDown('slow').find('p').eq(0).text(link);
+                        $('.risultati').find('div').eq(i).slideDown('slow').find('p').eq(1).html(snippet);
+                        $('.risultati').find('div').eq(i).slideDown('slow').find('p').eq(2).find('a').attr({'href': 'https://it.wikipedia.org/wiki/'+codificato, 'target': '_blank'});
                     }
 
                 } else {
