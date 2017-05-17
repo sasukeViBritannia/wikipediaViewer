31ebfc105667fc35e2963f4e2ef6dced828c9989
Ale <ale@aleLinux>
2017-05-17 22:59:13 +0200

    Aggiunta gestione errori e pulsante tondo, da rifinire styling

--- javascript/animazione.js
+++ javascript/animazione.js
@@ -11,25 +11,32 @@ $(document).ready(function() {
 
     $('form').submit(function(event) {
         event.preventDefault();
+        $('div.card').hide();
         var testo = $('input[name="ricerca"]').val();
-        window.alert('Form attivata con testo: ' + testo);
+        /*window.alert('Form attivata con testo: ' + testo);*/
         $.ajax({
                 url: 'https://en.wikipedia.org///w/api.php?action=query&format=json&list=search&utf8=1&srsearch=' + testo,
-                /*url: 'https://en.wikipedia.org///w/api.php?action=opensearch&format=json&limit=10&utf8=1&search='+testo,*/
                 type: 'GET',
                 dataType: 'jsonp',
             })
             .done(function(data) {
                 console.log("success");
-                for (var i = 0; i < 9; i++) {
-                    link = data.query.search[i].title;
-                    var snippet = data.query.search[i].snippet;
-                    var codificato = encodeURI(link);
-                    $('.risultati').append('<div class="card"><p>Titolo</p><p>Contenuto</p><p>' +
-                        '<a href="#">Continua su Wikipedia</a><span id="freccia" class="espandi testoNonEspanso"></span></p></div>');
-                    $('.risultati').find('div').eq(i).slideDown('slow').find('p').eq(0).text(link);
-                    $('.risultati').find('div').eq(i).slideDown('slow').find('p').eq(1).html(snippet);
-                    $('.risultati').find('div').eq(i).slideDown('slow').find('p').eq(2).find('a').attr({ 'href': 'https://en.wikipedia.org/wiki/' + codificato, 'target': '_blank' });
+                var trovati = data.query.searchinfo.totalhits;
+                if (trovati !== 0) {
+                    for (var i = 0; i < 9; i++) {
+                        link = data.query.search[i].title;
+                        var snippet = data.query.search[i].snippet;
+                        var codificato = encodeURI(link);
+                        $('.risultati').append('<div class="card"><p>Titolo</p><p>Contenuto</p><p>' +
+                            '<a href="#">Continua su Wikipedia</a><span id="freccia" class="espandi testoNonEspanso"></span></p></div>');
+                        $('.risultati').find('div').eq(i).slideDown('slow').find('p').eq(0).text(link);
+                        $('.risultati').find('div').eq(i).slideDown('slow').find('p').eq(1).html(snippet);
+                        $('.risultati').find('div').eq(i).slideDown('slow').find('p').eq(2).find('a').attr({ 'href': 'https://en.wikipedia.org/wiki/' + codificato, 'target': '_blank' });
+
+                    }
+
+                } else {
+                    window.alert('Nessuno trovato');
                 }
             })
             .fail(function() {
