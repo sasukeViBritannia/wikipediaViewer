13b342eaded4192bd152307645229d496c314f85
ale <ale@ubuntu>
2017-05-19 18:27:05 +0200

    Terminate le media query ed aggiunta gestione tasto enter, da fare styling

--- javascript/animazione.js
+++ javascript/animazione.js
@@ -7,12 +7,23 @@ $(document).ready(function() {
     /*Apertura pagina casuale in wikipedia*/
     $('#casuale').on('click', function() {
         window.open('https://it.wikipedia.org/wiki/Special:Random');
-    });
+    })
 
     $('form').submit(function(event) {
         event.preventDefault();
+    });
+
+//prevenire pressine tasto enter fa partire la query
+    $(document).keypress(
+        function(event) {
+            if (event.keyCode == '13') {
+                event.preventDefault();
+            }
+        });
+
+    $('#invia').on('click', function() {
         $('.risultati').empty();
-        $('.risultati').css('padding','0');
+        $('.risultati').css('padding', '0');
         var testo = $('input[name="ricerca"]').val();
         /*window.alert('Form attivata con testo: ' + testo);*/
         $.ajax({
@@ -28,7 +39,7 @@ $(document).ready(function() {
                         link = data.query.search[i].title;
                         var snippet = data.query.search[i].snippet;
                         var codificato = encodeURI(link);
-                        $('.risultati').css('padding','20px');
+                        $('.risultati').css('padding', '20px');
                         $('.risultati').append('<div class="card"><p>Titolo</p><p>Contenuto</p><p>' +
                             '<a href="#">Continua su Wikipedia</a><span id="freccia" class="espandi testoNonEspanso"></span></p></div>');
                         $('.risultati').find('div').eq(i).slideDown('slow').find('p').eq(0).text(link);
@@ -56,4 +67,3 @@ $(document).ready(function() {
 
 });
 
-/*Verificare differenza search con opensearch*/
